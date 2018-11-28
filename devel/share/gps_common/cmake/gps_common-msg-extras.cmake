cf676afa1bb1c60a5783e69f2da4d91a1e8a7b8d
kang <410557463@qq.com>
2018-11-29 02:48:59 +0800

    first

---
+++ devel/share/gps_common/cmake/gps_common-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(gps_common_MESSAGE_FILES "/home/kang/dev/rt3000/src/gps_umd/gps_common/msg/GPSStatus.msg;/home/kang/dev/rt3000/src/gps_umd/gps_common/msg/GPSFix.msg")
+set(gps_common_SERVICE_FILES "")
